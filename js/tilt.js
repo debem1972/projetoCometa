7c9a1cb80781b9fd375530d2d864e56e0115d78e
Daniel <debem1972@gmail.com>
2025-05-20 15:52:44 -0300

    Foram deletados os arquivos javaScript desnecessários ao projeto

--- js/tilt.js
+++
@@ -1,7 +0,0 @@
-// Inicializar Vanilla Tilt nas imagens dos cards
-VanillaTilt.init(document.querySelectorAll('.product-img'), {
-    max: 15, // Ângulo máximo de inclinação (menor que o exemplo para um efeito mais sutil)
-    speed: 400, // Velocidade da animação
-    glare: true, // Ativa o brilho
-    'max-glare': 0.3 // Intensidade do brilho (menor para não sobrecarregar visualmente)
-});
